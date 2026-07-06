599b9533c1c195839d8a8cabbea029af3e6c066f
Folk1910 <phanuwatfolk02@gmail.com>
2026-07-06 18:05:29 +0700

    เชื่อมต่อ mysql
    ติดตั้ง mysql และ เชื่อมต่อกับ backend

--- appcar/backend/api/app.js
+++ appcar/backend/api/app.js
@@ -5,9 +5,23 @@ var app = express();
 // Adds headers: Access-Control-Allow-Origin: *
 app.use(cors());
 
-app.get('/test', function (req, res, next) {
-  res.json({ msg: 'Hello world' });
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'appcar'
 });
+ 
+connection.connect();
+
+app.get('/test', function (req, res, next) {
+  connection.query('SELECT * FROM `information`', function (error, results, fields) {
+  if (error) throw error;
+    res.json(results);
+  });
+});
+
 
 app.listen(3333, function () {
   console.log('web server listening on port 3333');
